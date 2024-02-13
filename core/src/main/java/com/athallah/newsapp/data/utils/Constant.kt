192e2879e373203bf96be6e13e3e75ea0cf36eed
Athallah <athallahramadani21@gmail.com>
2024-02-13 18:09:35 +0700

    change api key

--- core/src/main/java/com/athallah/newsapp/data/utils/Constant.kt
+++ core/src/main/java/com/athallah/newsapp/data/utils/Constant.kt
@@ -5,7 +5,7 @@ class Constant {
         //API_KEY 1 = 531c8820cb514f238b7c2af60d62560d
         //API_KEY 2 = 876f883d026f493eba3405a77efc62b6
         //API_KEY 3 = d4d46e309bbb4e598b9f1ead2a579be5
-        const val API_KEY = "d4d46e309bbb4e598b9f1ead2a579be5"
+        const val API_KEY = "531c8820cb514f238b7c2af60d62560d"
         const val BASE_URL = "https://newsapi.org/"
     }
 }
